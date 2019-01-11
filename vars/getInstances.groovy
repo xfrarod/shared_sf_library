f56890b0bab5f65ffb25ab8cdc267165c234b018
mons3rrat <msede25@gmail.com>
2019-01-11 13:38:14 -0600

    Add new getInstance function

---
+++ vars/getInstances.groovy
@@ -0,0 +1,8 @@
+def ArrayList<String> call(){
+    def tkInstanceNames = []
+		def lines = sh(script: 'kitchen list', returnStdout: true).split('\n')
+			for (int i = 1; i < lines.size(); i++) {
+				tkInstanceNames << lines[i].tokenize(' ')[0]
+			}
+			return tkInstanceNames
+}
---
+++ vars/kitchenParallel.groovy
@@ -0,0 +1,22 @@
+def kitchenParallel() {
+    this.kitchenParallel(this.getInstances())
+}
+
+def call (ArrayList<String> instanceNames) {
+    def parallelNodes = [:]
+
+    for (int i = 0; i < instanceNames.size(); i++) {
+        def instanceName = instanceNames.get(i)
+        parallelNodes["tk-${instanceName}"] = {
+					result = sh(script: 'kitchen test --destroy always ' + instanceName, returnStatus: true)
+						if (result != 0) {
+							echo "kitchen returned non-zero exit status"
+							echo "Archiving test-kitchen logs"
+							archive(includes: ".kitchen/logs/${instanceName}.log")
+							error("kitchen returned non-zero exit status")
+						}
+				}
+    }
+
+    parallel parallelNodes
+}
