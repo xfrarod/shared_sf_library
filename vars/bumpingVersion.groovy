17146bfc432899215724065ffea1881f66bb3a6d
mons3rrat <msede25@gmail.com>
2019-01-16 09:41:54 -0600

    Adding cookbook name as a parameter

--- vars/bumpingVersion.groovy
+++ vars/bumpingVersion.groovy
@@ -1,6 +1,6 @@
-def call(){
-    def COOKBOOK_VERSION = sh(script: 'cat metadata.rb | grep ^version | awk '{print $2}' | sed "s|\'||g"')
-    def NEW_COOOKBOOK_VERSION = sh(script: 'echo $COOKBOOK_VERSION | awk -F. -v OFS=. \'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}\'')
+def call(String cookbook){
+    def COOKBOOK_VERSION = sh(script: 'cat cookbook/'${cookbook}'/metadata.rb | grep ^version | awk '{print $2}' | sed "s|\'||g"')
+    def NEW_COOOKBOOK_VERSION = sh(script: 'echo '${COOOKBOOK_VERSION}' | awk -F. -v OFS=. \'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}\'')
     sh """
         sed -i "s|version '${COOOKBOOK_VERSION}|version '${NEW_COOOKBOOK_VERSION}|g" metadata.rb
         git commit -am "Updating cookbook version to: '${NEW_COOOKBOOK_VERSION}'" &&\n git push origin HEAD:'${env.BRANCH_NAME}'
