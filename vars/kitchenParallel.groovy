e7b90a6ce523ad3ab9d076fbe60db2f8d3664e3d
Francisco Rodriguez <franciscoj.rodriguez@outlook.com>
2019-02-21 10:45:14 -0600

    Update kitchenParallel.groovy

--- vars/kitchenParallel.groovy
+++ vars/kitchenParallel.groovy
@@ -2,20 +2,21 @@
     this.kitchenParallel(this.getInstances())
 }*/
 
-def call (ArrayList<String> instanceNames) {
+def call (ArrayList<String> instanceNames, String cookbook) {
     def parallelNodes = [:]
 
     for (int i = 0; i < instanceNames.size(); i++) {
         def instanceName = instanceNames.get(i)
         parallelNodes["tk-${instanceName}"] = {
-					result = sh(script: 'cd cookbook/custom_nginx/; kitchen test --destroy always ' + instanceName, returnStatus: true)
-						if (result != 0) {
-							echo "kitchen returned non-zero exit status"
-							echo "Archiving test-kitchen logs"
-							archive(includes: ".kitchen/logs/${instanceName}.log")
-							error("kitchen returned non-zero exit status")
-						}
-				}
+		result = sh(script: "cd cookbook/${cookbook}/ && berks install && kitchen test --destroy always ${instanceName}", returnStatus: true)
+		
+		if (result != 0) {
+			echo "kitchen returned non-zero exit status"
+			echo "Archiving test-kitchen logs"
+			archive(includes: ".kitchen/logs/${instanceName}.log")
+			error("kitchen returned non-zero exit status")
+		}
+	}
     }
 
     parallel parallelNodes
